5aceea6bcb58fb53c644aefa2c8bec44aaca6514
rodbasso <bas18001@byui.edu>
2020-01-18 15:02:57 -0300

    update

--- lesson2/js/times.js
+++ lesson2/js/times.js
@@ -1,6 +1,6 @@
-// Current year Function
+// Return current year in numeric form
 const options = {year:'numeric'};
 document.getElementById('year').textContent = new Date().toLocaleDateString('en-US', options);
 
-//Date and time on which the document was last modified
+// Return the date and time on which the document was last modified
 document.getElementById('lastupdated').textContent = document.lastModified;
