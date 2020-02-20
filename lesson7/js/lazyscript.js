e8ad010c4cc9ff9e9f2d7ed3bf1e25272da601ed
rodbasso <bas18001@byui.edu>
2020-02-20 12:32:33 -0300

    up

--- lesson7/js/lazyscript.js
+++ lesson7/js/lazyscript.js
@@ -1,19 +1,15 @@
-// Return current year in numeric form
-const options = {year:'numeric'};
-document.getElementById('year').textContent = new Date().toLocaleDateString('en-US', options);
+  // Return current date in the format "Wednesday, 20 May 2020"
+  var currentDate = new Date();
+  var weekdayOption = { weekday: 'long' };
+  var weekday = currentDate.toLocaleDateString('en-US', weekdayOption);
+  var monthOption = { month: 'long' };
+  var month = currentDate.toLocaleDateString('en-US', monthOption);
+  var day = currentDate.getDate();
+  var year = currentDate.getFullYear();
+  
+  document.getElementById('currentdate').textContent = weekday + ", " + day + " " + month + " " + year;
 
-// Return the date and time on which the document was last modified. format "Monday, 01 Jan 2020"
-var currentDate = new Date();
-var weekdayOption = { weekday: 'long' };
-var weekday = currentDate.toLocaleDateString('en-US', weekdayOption);
-var monthOption = { month: 'long' };
-var month = currentDate.toLocaleDateString('en-US', monthOption);
-var day = currentDate.getDate();
-var year = currentDate.getFullYear();
-
-document.getElementById('currentdate').textContent = weekday + ", " + day + " " + month + " " + year;
-
-//*Lazy Load progressive image loading
+//*Lazy Load. progressive image loading
 let imagesToLoad = document.querySelectorAll("img[data-src]");
 
 const imgOptions = {
