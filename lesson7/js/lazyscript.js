d2f5ddc7ce346464aeafba3e7157b4a1729f98e8
rodbasso <bas18001@byui.edu>
2020-02-22 14:37:54 -0300

    update

--- lesson7/js/lazyscript.js
+++ lesson7/js/lazyscript.js
@@ -1,17 +1,28 @@
-  // Return current year in numeric form
-const options = {year:'numeric'};
+//hamburger menu class
+function toggleMenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+
+// Return current year in numeric form
+const options = {
+    year: 'numeric'
+};
 document.getElementById('year').textContent = new Date().toLocaleDateString('en-US', options);
 
-  // Return current date in the format "Wednesday, 20 May 2020"
-  var currentDate = new Date();
-  var weekdayOption = { weekday: 'long' };
-  var weekday = currentDate.toLocaleDateString('en-US', weekdayOption);
-  var monthOption = { month: 'long' };
-  var month = currentDate.toLocaleDateString('en-US', monthOption);
-  var day = currentDate.getDate();
-  var year = currentDate.getFullYear();
-  
-  document.getElementById('currentdate').textContent = weekday + ", " + day + " " + month + " " + year;
+// Return current date in the format "Wednesday, 20 May 2020"
+var currentDate = new Date();
+var weekdayOption = {
+    weekday: 'long'
+};
+var weekday = currentDate.toLocaleDateString('en-US', weekdayOption);
+var monthOption = {
+    month: 'long'
+};
+var month = currentDate.toLocaleDateString('en-US', monthOption);
+var day = currentDate.getDate();
+var year = currentDate.getFullYear();
+
+document.getElementById('currentdate').textContent = weekday + ", " + day + " " + month + " " + year;
 
 //*Lazy Load. progressive image loading
 let imagesToLoad = document.querySelectorAll("img[data-src]");
@@ -48,4 +59,11 @@ if ('IntersectionObserver' in window) {
     imagesToLoad.forEach((img) => {
         loadImages(img);
     });
+}
+
+//Banner
+function showBanner() {
+    if (weekday == 'Friday') {
+        document.getElementsByClassName("hidebanner")[0].classList.toggle("hidebanner");
+    }
 }
