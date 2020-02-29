d859563cfb7ae52a46a3803934b32f52c5b2bb82
rodbasso <bas18001@byui.edu>
2020-02-29 10:06:47 -0300

    update8

---
+++ lesson8/js/windchill.js
@@ -0,0 +1,17 @@
+function WindChill() {
+  var temp = document.getElementById("currtemp").innerHTML;
+  var windSpeed = document.getElementById("windspeed").innerHTML;
+  var speedFactor = Math.pow(windSpeed, 0.16);
+  var windChill = Math.round(35.74 + (0.6215 * temp) - 
+                  (35.75 * speedFactor) + 
+                  (0.4275 * temp * speedFactor));
+
+  if (temp <= 50 && windSpeed >= 3) {
+    document.getElementById("windchill").innerHTML = windChill;
+  }
+  else {
+    document.getElementById("windchilltag").innerHTML = "N/A";
+  }
+}
+
+WindChill()
