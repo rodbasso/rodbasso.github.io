f554671b4aaccf024024532d8df06811aa6a080a
rodbasso <bas18001@byui.edu>
2020-04-07 19:09:23 -0400

    up2

--- final-project/guides/js/team.js
+++ final-project/guides/js/team.js
@@ -1,18 +1,19 @@
-const requestURL = 'C:\Users\rbasso\iCloudDrive\01. Rod Basso\01. BYUI\AAS BYUI\AAS\06. WINTER SEMESTER 2020\CIT230\Lab\rodbasso.github.io\final-project\guides';
+const requestURL = 'https://github.com/rodbasso/rodbasso.github.io/tree/master/final-project/guides/team.json';
 
 fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject); // temporary checking for valid response and data parsing
-    const towns = jsonObject['towns'];
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const team = jsonObject['team'];
 
-    for (let i = 0; i < towns.length; i++) {
+    for (let i = 0; i < team.length; i++ ) {
       if (towns[i].name == "Fish Haven" ||
-        towns[i].name == "Preston" ||
-        towns[i].name == "Soda Springs") {
-        let card = document.createElement('section');
+          towns[i].name == "Preston" ||
+          towns[i].name == "Soda Springs")
+      {     
+        let card = document.createElement('section'); 
         let townInfo = document.createElement('div');
         let townImage = document.createElement('div');
         let name = document.createElement('h3');
@@ -24,14 +25,14 @@ fetch(requestURL)
 
         townInfo.className = "town-info";
         townImage.className = "town-image";
-        name.textContent = towns[i].name;
-        motto.textContent = towns[i].motto;
+        name.textContent = team[i].name;
+        motto.textContent = team[i].motto;
         motto.className = "motto";
-        founded.textContent = 'Year Founded: ' + towns[i].yearFounded;
-        population.textContent = 'Population: ' + towns[i].currentPopulation;
-        rainfall.textContent = 'Annual Rain Fall: ' + towns[i].averageRainfall;
-        photo.setAttribute('src', "images/" + towns[i].photo);
-        photo.setAttribute('alt', towns[i].name + ' ' + towns[i].lastname + ' - ' + (i + 1));
+        founded.textContent = 'Year Founded: ' + team[i].yearFounded;
+        population.textContent = 'Population: ' + team[i].currentPopulation;
+        rainfall.textContent = 'Annual Rain Fall: ' + team[i].averageRainfall;
+        photo.setAttribute('src', "images/" + team[i].photo);
+        photo.setAttribute('alt', team[i].name + ' ' + team[i].lastname + ' - ' + (i + 1));
 
         townInfo.appendChild(name);
         townInfo.appendChild(motto);
