cef6cfb0b501094d141a2bec35fd47184ee5d634
rodbasso <bas18001@byui.edu>
2020-04-07 22:21:07 -0400

    up

--- final-project/guides/js/team.js
+++ final-project/guides/js/team.js
@@ -1,4 +1,4 @@
-const requestURL = 'https://rodbasso.github.io/final-project/guides/teamdata.json ';
+const requestURL = 'https://rodbasso.github.io/final-project/guides/towndata.json ';
 
 fetch(requestURL)
   .then(function (response) {
